b9e60206b002e4b2b163927c35e087ac051c89fa
gifariarsal <117580307+gifariarsal@users.noreply.github.com>
2024-06-23 17:02:26 +0800

    feat: add assets in public folder, add Button and Navbar components

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,9 +1,42 @@
-import React from 'react'
+import { NAV_LINKS } from '@/constants';
+import Image from 'next/image';
+import Link from 'next/link';
+import Button from './Button';
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
-  )
-}
+    <nav className="flexBetween max-container padding-container relative z-30 py-5">
+      <Link href="/">
+        <Image src="/hilink-logo.svg" alt="logo" width={72} height={20} />
+      </Link>
+      <ul className="hidden h-full gap-12 lg:flex">
+        {NAV_LINKS.map(({ href, key, label }) => (
+          <Link
+            href={href}
+            key={href}
+            className="regular-16 text-gray-50 flexCenter cursor-pointer transition-all hover:text-green-50 hover:underline"
+          >
+            {label}
+          </Link>
+        ))}
+      </ul>
+      <div className="lg:flexCenter hidden">
+        <Button
+          type="button"
+          title="Login"
+          icon="/user.svg"
+          variant="btn_dark_green"
+        />
+      </div>
+      <Image
+        src="/menu.svg"
+        alt="menu"
+        width={24}
+        height={24}
+        className="inline-block cursor-pointer lg:hidden"
+      />
+    </nav>
+  );
+};
 
-export default Navbar
+export default Navbar;
