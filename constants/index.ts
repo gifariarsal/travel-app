89b3e30f6417945816f775189833965fe2c0984d
gifariarsal <117580307+gifariarsal@users.noreply.github.com>
2024-06-24 22:29:58 +0800

    feat: add Footer, edit data in constant, delete unused assets

--- constants/index.ts
+++ constants/index.ts
@@ -51,18 +51,11 @@ export const FEATURES = [
 export const FOOTER_LINKS = [
   {
     title: 'Learn More',
-    links: [
-      'About Hilink',
-      'Press Releases',
-      'Environment',
-      'Jobs',
-      'Privacy Policy',
-      'Contact Us',
-    ],
+    links: ['About', 'Environment', 'Jobs', 'Privacy Policy', 'Contact Us'],
   },
   {
     title: 'Our Community',
-    links: ['Climbing xixixi', 'Hiking hilink', 'Hilink kinthill'],
+    links: ['Climbing', 'Hiking', 'Camping'],
   },
 ];
 
@@ -70,17 +63,11 @@ export const FOOTER_CONTACT_INFO = {
   title: 'Contact Us',
   links: [
     { label: 'Admin Officer', value: '123-456-7890' },
-    { label: 'Email Officer', value: 'hilink@akinthil.com' },
+    { label: 'Email Officer', value: 'admin@hilink.com' },
   ],
 };
 
 export const SOCIALS = {
   title: 'Social',
-  links: [
-    '/facebook.svg',
-    '/instagram.svg',
-    '/twitter.svg',
-    '/youtube.svg',
-    '/wordpress.svg',
-  ],
+  links: ['/facebook.svg', '/instagram.svg', '/youtube.svg'],
 };
